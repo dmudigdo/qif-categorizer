c986c075d070ad1fa2c10e2e68bcdc131f1d8ac5
ddgallichroum <didi@didi.com.au>
2018-05-28 16:51:42 +0800

    finished version 2 but untested

--- qif-categorizer2.py
+++ qif-categorizer2.py
@@ -16,11 +16,44 @@ for line in catfile:
 catfile.close
 print categ
 
-# read qif file
 #open i/o files
-#for line in file:
+# Open input file
+ifile = open('input.qif')
+
+# Open output file
+ofile = open('output.qif', 'w')
+
+for line in file:
 #    if description-line:
-#        for key in categ:
-#            if key in line:
-#                add categ[key]
-#close files
+    if line.startswith('P'):
+        # It is a description field, so:
+        # 1. Write out that descriptionline
+        ofile.write(line)
+
+        # 2a. Scan the line for 'COLES' or 'WOOLWORTHS'.
+        #    If found, category is Groceries, write this category line to output file
+        # DELETE THIS BLOCK ONCE YOU ARE DONE:
+#        if 'COLES' in line or 'WOOLWORTHS' in line or 'CARLISLE IGA' in line:
+#            print('Found COLES or WOOLWORTHS or CARLISLE IGA')
+#            category = "Living Expenses:Groceries and Household Consumables"
+#            ofile.write('L'+category+'\n')
+        # endif
+
+        # THIS BLOCK REPLACES THE ABOVE:
+        # for each key in categ:
+        for key in categ:
+            if key in line:
+                print('found ' + key)
+                ofile.write('L'+ categ[key]) +'\n')
+            # endif
+         #endfor
+
+    else:
+    # else, it is not a description field, so write it straight out into the output file
+        ofile.write(line)
+    # endif
+#endfor
+
+# Close the files
+ifile.close()
+ofile.close()
