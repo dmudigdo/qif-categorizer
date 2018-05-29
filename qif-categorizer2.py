cbc0ccfa4e94875f61d66a1b240d58e487e762c8
ddgallichroum <didi@didi.com.au>
2018-05-29 11:39:42 +0800

    made it work

--- qif-categorizer2.py
+++ qif-categorizer2.py
@@ -3,53 +3,53 @@
 # keyphrase, category
 # e.g. "Guzman"\t"Eating Out"
 
-# Open categories file
+# open categories file
 catfile = open('categories.txt')
 
-# Populate a dictionary with the keyword -> category hash values
+# populate a dictionary with the (keyword -> category) hash values
 categ=dict()
 for line in catfile:
     content = line.split("\t")
     categ[content[0]]=content[1].rstrip()
 
-# Done
+# done 
 catfile.close
-print categ
 
-#open i/o files
-# Open input file
+# report categories
+print ''
+print "Categories found:"
+for key in categ:
+    print key + ' - ' + categ[key]
+
+print ''
+print 'Starting categorization...'
+print ''
+
+# open input file
 ifile = open('input.qif')
 
-# Open output file
+# open output file
 ofile = open('output.qif', 'w')
 
-for line in file:
-#    if description-line:
+for line in ifile:
+#   if it is a description-line:
     if line.startswith('P'):
         # It is a description field, so:
         # 1. Write out that descriptionline
         ofile.write(line)
 
-        # 2a. Scan the line for 'COLES' or 'WOOLWORTHS'.
-        #    If found, category is Groceries, write this category line to output file
-        # DELETE THIS BLOCK ONCE YOU ARE DONE:
-#        if 'COLES' in line or 'WOOLWORTHS' in line or 'CARLISLE IGA' in line:
-#            print('Found COLES or WOOLWORTHS or CARLISLE IGA')
-#            category = "Living Expenses:Groceries and Household Consumables"
-#            ofile.write('L'+category+'\n')
-        # endif
-
-        # THIS BLOCK REPLACES THE ABOVE:
-        # for each key in categ:
+        # 2. for each key in the categ dictionary:
         for key in categ:
+            # find if the key is in the description line
             if key in line:
-                print('found ' + key)
-                ofile.write('L'+ categ[key]) +'\n')
+                # and if found, do the magic
+                print('found ' + key + ', assigned to ' + categ[key])
+                ofile.write('L'+ categ[key] +'\n')
             # endif
          #endfor
 
     else:
-    # else, it is not a description field, so write it straight out into the output file
+    # else, it is not a description field, so duplicate it into the output file
         ofile.write(line)
     # endif
 #endfor
